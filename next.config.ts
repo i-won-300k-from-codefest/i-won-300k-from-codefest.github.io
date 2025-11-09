cce3a88d34aaa653fce212ad00bc7d7b08087080
wolf-yuan <wolf@wolf-yuan.dev>
2025-11-09 09:55:57 +0800

    feat: add basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: '/taipeipass-frontend'
+    assetPrefix: '/taipeipass-frontend/'
 };
 
 export default nextConfig;
